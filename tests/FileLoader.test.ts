ea91ea7bd6ec63928f21418a92cdc7b8cc455a02
Daan Klarenbeek <github@ijskoud.dev>
2023-05-22 14:37:41 +0200

    fix(Tests): incorrect import

--- tests/FileLoader.test.ts
+++ tests/FileLoader.test.ts
@@ -1,5 +1,5 @@
 import { join } from "node:path";
-import FileLoader from "../src/lib/Handlers/FileLoader.js";
+import FileLoader from "../src/lib/handlers/FileLoader.js";
 import { Collection } from "@discordjs/collection";
 
 function isClass(input: unknown) {
